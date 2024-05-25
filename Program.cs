d34a66a44be18542fff629dd98ee3fe758a7df4b
0ankit0 <ankitpaudel.1373@gmail.com>
2024-05-25 08:25:48 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+//https://learn.microsoft.com/en-us/aspnet/core/fundamentals/middleware/?view=aspnetcore-8.0#order for info on middleware
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Add WebOptimizer to allow minification and bundling of CSS and JS files
+//check: https://github.com/ligershark/WebOptimizer?tab=readme-ov-file for more details
+builder.Services.AddWebOptimizer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+// Add WebOptimizer to the request pipeline 
+app.UseWebOptimizer();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
