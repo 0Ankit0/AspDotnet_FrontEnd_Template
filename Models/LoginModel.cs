aabe0de2ebe1c688a05b1a31345b2439332012ee
0ankit0 <ankitpaudel.1373@gmail.com>
2024-05-29 08:03:36 +0545

    chore: Add LoginController and LoginModel

---
+++ Models/LoginModel.cs
@@ -0,0 +1,8 @@
+namespace Template.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -31,6 +31,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}");
 
 app.Run();
