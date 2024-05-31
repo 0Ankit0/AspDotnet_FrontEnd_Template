924d57b67f4b1d25f6b9c9810294dd8674ab45bf
0ankit0 <ankitpaudel.1373@gmail.com>
2024-05-31 07:45:30 +0545

    chore: Add Apicall class for making API calls and update Program.cs to configure the Apicall class as a singleton

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Template.Classes;
+
 //https://learn.microsoft.com/en-us/aspnet/core/fundamentals/middleware/?view=aspnetcore-8.0#order for info on middleware
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +17,12 @@ builder.Services.AddResponseCaching(options =>
 //maximum body size is the maximum size of the response body that can be cached
 //use case sensitive paths is a boolean that determines if the paths are case sensitive
 //size limit is the maximum size of the response that can be cached
+
+// Read the connection string
+var baseAddress = builder.Configuration.GetConnectionString("BaseAddress");
+
+// Add the Apicall class as a singleton
+builder.Services.AddSingleton<IApiCall,Apicall>();
 
 // Add WebOptimizer to allow minification and bundling of CSS and JS files
 //check: https://github.com/ligershark/WebOptimizer?tab=readme-ov-file for more details
