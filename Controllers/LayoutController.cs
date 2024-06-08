2fb0678dce54543c952a28bce2b779c8b185b506
0ankit0 <ankitpaudel.1373@gmail.com>
2024-06-08 08:24:42 +0545

    chore: Add LayoutController for handling layout views

---
+++ Controllers/LayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Template.Controllers
+{
+    public class LayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Templates/ModelTemplate.cs
@@ -0,0 +1,8 @@
+﻿namespace Template.Models
+{
+    public class Model
+    {
+        public string TokenNo { get; set; }
+        public string GUID { get; set; }
+    }
+}
