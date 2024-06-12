73dcfee1b302e4271a1aba198f185ee747555bb2
0ankit0 <ankitpaudel.1373@gmail.com>
2024-06-12 07:01:47 +0545

    feat: created template to create model based on database table

--- Templates/ModelTemplate.cs
+++ Templates/ModelTemplate.cs
@@ -1,10 +1,16 @@
-﻿
-
-namespace Template.Models
-{
-    public class CustomModel
+﻿namespace Template.Models{
+    public class Users
     {
-        public string TokenNo { get; set; }
-        public string GUID { get; set; }
+     public long UserId { get; set; }
+     public string UserName { get; set; }
+     public string UserEmail { get; set; }
+     public string Password { get; set; }
+     public string Address { get; set; }
+     public string Phone { get; set; }
+     public string GUID { get; set; }
+     public DateTime CreatedAt { get; set; }
+     public DateTime UpdatedAt { get; set; }
+     public string Role { get; set; }
+     public bool Active { get; set; }
     }
 }
