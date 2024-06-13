d7db73fd4c3f8dfddba8356cf20df827240d4d35
0ankit0 <ankitpaudel.1373@gmail.com>
2024-06-13 06:57:44 +0545

    chore: Add TemplateController and TemplateModel classes for managing templates

---
+++ Controllers/TemplateController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Template.Controllers
+{
+    public class TemplateController1 : Controller
+    {
+        // GET: TemplateController1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TemplateController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TemplateController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TemplateController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TemplateController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TemplateController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TemplateController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TemplateController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/TemplateModel.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Template.Models
+{
+    public class TemplateModel
+    {
+        public string connString { get; set; }
+        public string tblName { get; set; }
+    }
+    
+
+}
